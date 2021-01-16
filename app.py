4d28c20f0e344135decb38c9c117c7fdbca47ca9
Joshua Rhoads <josh@MacBook-Pro.local>
2021-01-16 15:13:57 -0600

    final update to repo

--- app.py
+++ app.py
@@ -1,86 +1,60 @@
-import io
-import sys
-import csv
+
+import pandas as pd
 import os
-import uuid
-import csv
 import json
-import pymongo
-from pymongo import MongoClient
+from datetime import date
 from pprint import pprint
-import datetime
-import flask
-from flask import (
-    Flask,
-    flask_pymongo,
-    render_template,
-    jsonify,
-    request,
-    redirect,)
-import bson
-from bson.json_util import dumps
-from flask_pymongo import PyMongo
+
+import requests
+import csv
+
+from flask import Flask, redirect, jsonify, render_template, url_for
+
+import pandas as pd
+
+import geopy.distance
+import requests
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, and_
+from api_keys import database_uri
 
 
+## # Import database user and password
+try:
+    from api_keys import pg_host
+    from api_keys import db_name
+    from api_keys import pg_user
+    from api_keys import pg_pass
+except:
+    pass
 
 
-# Use PyMongo to establish Mongo connection
+#Database name and database tables
 
-# try:
-#     uri = os.environ["MONGODB_URI"]
-    
-
+table_airplanes = "aircraft_data"
+airportData = "airportData"
 
 
-#Import database user and password
-
-
-
-
-# Database name and database tables
-
- 
 
 # MySQL specific connection string
 
+connection_string = f"{pg_user}:{pg_pass}@{pg_host}/{db_name}"
+engine = create_engine(f'postgresql://{connection_string}')
 
 
-# Create the engine to connect to the database
-###############################################
-# Flask Setup
-#################################################
-conn = ['MONGO_URI']
-client = PyMongo(conn)
+# Query database for airport data that will be always the same
 
-# Use PyMongo to establish Mongo connection
+airport_db = db_name.find(airportData)
 
-db = client.flight
-flightData = db.flight
-# cursor = collection.find({})
-# with open('collection.json', 'w') as file:
-#         file.write('[')
-#         for document in cursor:
-#             file.write(load(document))
+jsonpath = os.path.join("data","airports.json")
+with open(jsonpath) as f:
+    data = json.loads(f.read())
+    # print(data[0]['text'])
+# if isinstance(data, list):
+#     db_name.insert(data)
 
-
-
-# flight = mongo.db
-# flightData = flight.create_collection
-
-
-#################################################
-# Flask Routes
-#################################################
-# Use Flask to create your routes.
-
-# Route
-# jsonpath = os.path.join("collection.json")
-# with open(jsonpath, 'r') as datafile:
-#     data = json.loads(datafile)
-#     if isinstance(data, list):
-#         flightData.insert_many(data)
-#     else:
-#         flightData.insert_one(data)
 
 
 
@@ -88,18 +62,13 @@ flightData = db.flight
 # Flask Setup
 #################################################
 app = Flask(__name__)
-# app.wsgi_app = ProxyFix(app.wsgi_app)
 
 
 #################################################
 # Flask Routes
 #################################################
 # Use Flask to create your routes.
-@app.route('/users')
-def users():
-        user = db.flightData.find()
-        resp = dumps(user)
-        return resp
+
 
 # Home page.
 @app.route("/")
@@ -110,16 +79,16 @@ def home():
     return render_template("index_Josh_v6.html")
 @app.route("/api")
 def api():
-    return render_template("index_api.html")
-@app.route("/radar")
-def radar():
+    return render_template("index_api.html")   
+@app.route("/radar")    
+def radar(): 
     return render_template("indexradar.html")
-@app.route("/test")
-def test():
+@app.route("/test")    
+def test(): 
     return render_template("index_Josh_v5.html")
 @app.route("/weather")
 def weather():
-    return redirect('http://localhost:5001', code=301)
+    return redirect('http://localhost:5001', code=301)    
 
 # Return the APIs route available
 @app.route("/api/v1.0")
@@ -134,172 +103,122 @@ def airports_url():
         print(records)
         return jsonify(parsed)
     
-# Return a json with the query results for the airports table
-@app.route("/api/v1.0/airports-data")
-def api_airports():
-    api_airports = ("collection.json")
-    
-    list_records = []
-    for records in api_airports:
-        Data = list_records.append(records)
+       
         
-        parsed = json.dumps(Data)
-        print(records)
-        return jsonify(parsed)
+# if __name__ == "__main__":
+#     app.run(debug=True)
 
+
+
+# Return a json with the query results for the aircrafts table
+@app.route("/api/v1.0/aircrafts-data")
+def api_aircrafts():
+    # MySQL query to return all table elements that have not 
+    # null latitute and have the newest time stamp
+    aircraft_df = pd.read_sql(
+        f"""
+        SELECT
+            * 
+        FROM 
+            {table_airplanes}
+        WHERE 
+            longitude IS NOT NULL
+        AND 
+            time = (SELECT MAX(time) 
+        FROM 
+            {table_airplanes})
+        ORDER BY 
+            id
+        DESC;
+        """,
+         engine)
+
+    result = aircraft_df.to_json(orient="records")
+    parsed = json.loads(result)
+
+    return jsonify(parsed)
+
+# Return a json with the query results for the airports table
+@app.route("/api/v1.0/airports-data/<country>")
+def api_airports(country):
+
+    if f"{country}" == 'ALL':
+        airports_df = airportData
+        
+    else:
+        airports_df = airports_df_all.loc[airportsDatal['Country']==f"{country}"]
+    result = airports_df.to_json(orient="records")
+    parsed = json.loads(result)
+
+    return jsonify(parsed)
 
 # Return a json with the query results for the aircrafts table for a specific icao24
-@app.route("/api/v1.0/flightData/icao24/<icao24>")
-def api_aircrafts_icao24(icao24):
+@app.route("/api/v1.0/aircrafts-data/icao24/<icao24>")
+def api_aircrafts_icao24():
 
-    # Create place holders records to insert into the table
-                           
-                    MongoStuff =       {"id"                   ,
-                                        "icao24",
-                                        "callsign",
-                                        "origin_country",
-                                        "time_position",
-                                        "last_contact",
-                                        "longitude",
-                                        "latitude",
-                                        "baro_altitude",
-                                        "on_ground",
-                                        "velocity",
-                                        "true_track",
-                                        "vertical_rate",
-                                        "sensors",
-                                        "geo_altitude",
-                                        "squawk",
-                                        "spi",
-                                        "position_source",
-                                        "time"}
-           
+    aircraft_df = pd.read_sql(
+        f"""
+        SELECT 
+            *
+        FROM
+            {table_airplanes}
+        WHERE
+            longitude IS NOT NULL 
+        AND
+            icao24 = '{str(icao24)}';
+        """,
+         engine)
 
+    result = aircraft_df.to_json(orient="records")
+    parsed = json.loads(result)
+    return jsonify(parsed)
 
-# Save the record to the database
-# single element at the time
-def add_to_database(flight):
-    try:
-        # Add records to the database
-        flight.execute(MongoStuff, flightData)
-
-        # Commit changes to the database
-        db.commit()
-
-        # print('Record Added to DB')
-    except:
-        print('Error Saving to DB')
-
-
-
-# Save the record to the database with
-# multiple records at the same time
-def add_many_database(flightData):
-    try:
-        # Add records to the database
-        my_cursor.executemany(MongoStuff, data)
-
-        # Commit changes to the database
-        db.commit()
-
-        print('Record Added to DB')
-    except:
-        print('Error Saving to DB')
-        
-if __name__ == "__main__":
-    app.run(debug=True)
-
-# try:
-#     # Save config information.
-#     url = "https://opensky-network.org/api/states/all?time=1458564121&icao24=3c6444"
-
-
-#     # Build partial query URL
-#     # query_url = f"{url}"
-
-#     #  Perform a request for data
-    
-#     response = requests.get(url).json()
-#     return response
-#     list_records = []
-#     # Test for null and strip spaces in case not null
-#     for i in range(len(response["states"])):
-#         aircraft_live_data = list(range(0,18))
-#         if response["states"][i][0]:
-#             aircraft_live_data[0] = response["states"][i][0].strip()
-#         else:
-#             aircraft_live_data[0] = response["states"][i][0]
-#         if response["states"][i][1]:
-#             aircraft_live_data[1] = response["states"][i][1].strip()
-#         else:
-#             aircraft_live_data[1] =  response["states"][i][1]
-#         if response["states"][i][2]:
-#             aircraft_live_data[2] =  response["states"][i][2].strip()
-#         else:
-#             aircraft_live_data[2] =  response["states"][i][2]
-#         aircraft_live_data[3] =  response["states"][i][3]
-#         aircraft_live_data[4] =  response["states"][i][4]
-#         aircraft_live_data[5] =  response["states"][i][5]
-#         aircraft_live_data[6] =  response["states"][i][6]
-#         aircraft_live_data[7] =  response["states"][i][7]
-#         aircraft_live_data[8] =  response["states"][i][8]
-#         aircraft_live_data[9] =  response["states"][i][9]
-#         aircraft_live_data[10] =  response["states"][i][10]
-#         aircraft_live_data[11] =  response["states"][i][11]
-#         aircraft_live_data[12] =  response["states"][i][12]
-#         aircraft_live_data[13] =  response["states"][i][13]
-#         if response["states"][i][14]:
-#             aircraft_live_data[14] =  response["states"][i][14].strip()
-#         else:
-#             aircraft_live_data[14] =  response["states"][i][14]
-#         aircraft_live_data[15] =  response["states"][i][15]
-#         aircraft_live_data[16]  = response["states"][i][16]
-#         aircraft_live_data[17] = response["time"]
-
-#         record1 = (aircraft_live_data[0], 
-#                     aircraft_live_data[1],
-#                     aircraft_live_data[2],
-#                     aircraft_live_data[3],
-#                     aircraft_live_data[4],
-#                     aircraft_live_data[5],
-#                     aircraft_live_data[6],
-#                     aircraft_live_data[7],
-#                     aircraft_live_data[8],
-#                     aircraft_live_data[9],
-#                     aircraft_live_data[10],
-#                     aircraft_live_data[11],
-#                     aircraft_live_data[12],
-#                     aircraft_live_data[13],
-#                     aircraft_live_data[14],
-#                     aircraft_live_data[15],
-#                     aircraft_live_data[16],
-#                     aircraft_live_data[17]
-#                     )
-#         list_records.append(record1)
-    
-#     print("---------------------")
-#     dt_object = datetime.fromtimestamp(response["time"])
-#     print(f"At: {dt_object} | Total data points retrieved: {len(response['states'])}")
-
-
-# except:
-#     print("Failed to retrieve data")
 
 # Return a json with the query results for the aircrafts table for a specific callsign
-# @app.route("/api/v1.0/aircrafts-data/callsign/<callsign>")
-# def api_aircrafts_callsign(callsign):
+@app.route("/api/v1.0/aircrafts-data/callsign/<callsign>")
+def api_aircrafts_callsign():
 
-    # list_records = []
-    # for records in collection:
-    # Data = list_records.append(records)
-        
-        
-    # parsed = json.dumps(Data)
-    # print(records)
-    # return jsonify(parsed)
+    aircraft_df = pd.read_sql(
+        f"""
+        SELECT 
+            *
+        FROM
+            {table_airplanes}
+        WHERE
+            longitude IS NOT NULL
+        AND
+            callsign = '{str(callsign)}';
+        """,
+         engine)
+
+    result = aircraft_df.to_json(orient="records")
+    parsed = json.loads(result)
+    return jsonify(parsed)
 
 
+# Return a json with the query results for the aircrafts table for a specific callsign
+@app.route("/api/v1.0/aircrafts-data/byhour")
+def api_aircrafts_byhour():
 
+    aircraft_hour = pd.read_sql(
+        f"""
+        SELECT 
+            COUNT(*) AS totalDataPoints,
+            FROM_UNIXTIME(time, '%Y-%m-%d %H') AS timeData
+        FROM
+            {table_airplanes}
+        WHERE
+            longitude IS NOT NULL
+        GROUP BY FROM_UNIXTIME(time, '%Y-%m-%d %H');
+        """,
+         engine)
+
+    result = aircraft_hour.to_json(orient="records")
+    parsed = json.loads(result)
+    return jsonify(parsed)
 
 # The server is set to run on the computer IP address on the port 5100
 # Go to your http://ipaddress:5100
+if __name__ == "__main__":
+    app.run(debug=True)
+
